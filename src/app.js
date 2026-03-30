6e188ed5aca880f125372e04038f6be622456008
Simha <simhammu329789@gmail.com>
2026-03-30 17:30:19 +0530

    exploring get , post , delete

--- src/app.js
+++ src/app.js
@@ -4,17 +4,23 @@
 
  
 
- app.use("/hello",(req,res) => {
-    res.send("Hello from the server");
+ app.get("/user", (req,res) => {
+      res.send({ firstname: "Simha" , lastname: "A"})
+ });
+
+ app.post("/user", (req,res) =>{
+      res.send("Data Successfully saved to database")
+ });
+
+ app.delete("/user", (req,res) =>{
+   res.send("Deleted Successfully")
  });
 
  app.use("/test",(req,res) =>{
     res.send("Hello , you can test the server");
  });
 
- app.use("/", (req,res)=>{
-    res.send("Hello from dashboard");
- });
+ 
 
  app.listen(7777, () => {
     console.log("Server is running successfully on port 7777");
