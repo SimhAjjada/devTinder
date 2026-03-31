86e548185f5b9d46eeeca4bac18a4a2d73a782a4
Simha <simhammu329789@gmail.com>
2026-03-31 14:24:39 +0530

    playing with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -4,9 +4,27 @@
 
  
 
- app.get("/user/:userId/:name/:password", (req,res) => {
-      res.send({ firstname: "Simha" , lastname: "A"})
- });
+ app.get("/user", (req, res, next) => {
+      console.log("Handling the route user!!");
+      next();
+ },
+ (req, res, next) => {
+      console.log("Handling the route user 2!!");
+      next(); 
+ },
+ (req, res,next) => {
+   console.log("Handling the route user 3!!");
+   next();
+ },
+ (req, res, next) => {
+   console.log("Handling the route user 4!!");
+   next();
+ },
+ (req, res) => {
+   console.log("Handling the route user 5!!");
+   res.send("5th response");
+ }
+);
 
  
 
